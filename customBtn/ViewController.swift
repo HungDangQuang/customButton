0aac98351e765a335bc963e41e1a18e00317cc74
HungDangQuang <hungdangkg@gmail.com>
2021-12-06 17:18:14 +0700

    commit task

--- customBtn/ViewController.swift
+++ customBtn/ViewController.swift
@@ -12,15 +12,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let customBtn = customButton(frame: CGRect(x: 100, y: 100, width: 100, height: 40))
-        customBtn.setImage(image: UIImage(systemName: "house")!)
-        customBtn.setTitle(title: "hung")
-        customBtn.addTarget(self, action: #selector(pressed), for: .touchUpInside)
+//        let customBtn = customButton(frame: CGRect(x: 100, y: 100, width: 100, height: 40))
+//        customBtn.setImage(image: UIImage(systemName: "house")!)
+//        customBtn.setTitle(title: "hung")
+//        customBtn.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+//        self.view.addSubview(customBtn)
+                
+        let customBtn = customButton(frame: CGRect(x: 100, y: 100, width: 100, height: 40), title: "Click Me", selectedImage: UIImage(systemName: "house")!)
+        customBtn.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         self.view.addSubview(customBtn)
-            
+        
+        let btn2 = customButton(frame: CGRect(x: 100, y: 150, width: 100, height: 40), title: "Button 2")
+        btn2.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        self.view.addSubview(btn2)
+        
+        let btn3 = customButton(frame: CGRect(x: 100, y: 200, width: 100, height: 40),selectedImage: UIImage(systemName: "house")!)
+        btn3.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        self.view.addSubview(btn3)
+        
+        
     }
     @objc func pressed(){
         print("Button Pressed")
+    }
+    
+    @objc func buttonTapped(sender: customButton){
+        sender.showAnimation {
+            print("hello")
+        }
     }
     
 }
--- customBtn/customBtn.swift
+++ customBtn/customBtn.swift
@@ -17,6 +17,45 @@ class customButton: UIControl {
     super.init(frame: frame)
     self.backgroundColor = .gray
     }
+    convenience init(frame: CGRect,title: String, selectedImage: UIImage) {
+        
+        self.init(frame: frame)
+        
+        let lbl = UILabel()
+        lbl.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        lbl.text = title
+        lbl.textAlignment = .center
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        self.attributedTitle = lbl
+        self.addSubview(attributedTitle!)
+        
+        self.currentImage = UIImageView(image: selectedImage, highlightedImage: nil)
+        self.currentImage?.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(currentImage!)
+        
+        setUpLayout()
+    }
+    
+    convenience init(frame: CGRect, title: String) {
+        
+        self.init(frame: frame)
+        let lbl = UILabel()
+        lbl.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        lbl.text = title
+        lbl.textAlignment = .center
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        self.attributedTitle = lbl
+        self.addSubview(attributedTitle!)
+        setUpLayout()
+    }
+    
+    convenience init(frame: CGRect, selectedImage: UIImage) {
+        self.init(frame: frame)
+        self.currentImage = UIImageView(image: selectedImage, highlightedImage: nil)
+        self.currentImage?.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(currentImage!)
+        setUpLayout()
+    }
 
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
@@ -32,6 +71,7 @@ class customButton: UIControl {
         self.addSubview(attributedTitle!)
         setUpLayout()
 
+        
     }
     
     func setImage(image: UIImage){
@@ -97,5 +137,24 @@ extension UIView {
                 subview.clearConstraints()
             }
             self.removeConstraints(self.constraints)
+        }
+    func showAnimation(_ completionBlock: @escaping () -> Void) {
+          isUserInteractionEnabled = false
+            UIView.animate(withDuration: 0.1,
+                           delay: 0,
+                           options: .curveLinear,
+                           animations: { [weak self] in
+                                self?.transform = CGAffineTransform.init(scaleX: 0.95, y: 0.95)
+            }) {  (done) in
+                UIView.animate(withDuration: 0.1,
+                               delay: 0,
+                               options: .curveLinear,
+                               animations: { [weak self] in
+                                    self?.transform = CGAffineTransform.init(scaleX: 1, y: 1)
+                }) { [weak self] (_) in
+                    self?.isUserInteractionEnabled = true
+                    completionBlock()
+                }
+            }
         }
 }
